9bd95bc1038b9491f0f15546698b7f5ee2fbecff
Wipersee <kesterera@gmail.com>
2020-04-17 11:09:27 +0300

    created populate file

---
+++ populate.sql
@@ -0,0 +1,31 @@
+insert into companies values (1,'Nintendo');
+insert into companies values (2,'Microsoft Game Studios');
+insert into companies values (3,'Take-Two Interactive');
+
+insert into genre values(1,'Sports');
+insert into genre values(2,'Platform');
+insert into genre values(3,'Racing');
+insert into genre values(4,'Misc');
+insert into genre values(5,'Action');
+
+
+insert into platform values (1,'Wii');
+insert into platform values (2,'NES');
+insert into platform values (3,'XB360');
+insert into platform values (4,'PS3');
+
+insert into release_year values(1,2006);
+insert into release_year values(2,1985);
+insert into release_year values(3,2009);
+insert into release_year values(4,2010);
+insert into release_year values(5,2013);
+
+
+insert into game values(1,1,1,1,1,'Wii Sports');
+insert into sales values(1,41.49,29.02,3.77,8.46);
+
+insert into game values(1,1,1,1,1,'Super Mario Bros.');
+insert into sales values(1,41.49,29.02,3.77,8.46);
+
+insert into game values(2,2,1,2,2,'Super Mario Bros.');
+insert into sales values(1,29.08,3.58,6.81,0.77);
