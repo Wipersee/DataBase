179102ac5c83d63adb9740813962c66390ed3e52
Wipersee <kesterera@gmail.com>
2020-04-18 23:13:20 +0300

    added query.sql

--- query.sql
+++ query.sql
@@ -5,15 +5,16 @@ inner join sales using(game_rank)
 GROUP BY realease_year;
 
 
-select company
-,ROUND(sum(NA_sales)/(sum(NA_sales)+sum(EU_sales)+sum(JP_sales)+sum(Other_sales))*100,2)||'%' as NA_sales
-,ROUND(sum(EU_sales)/(sum(NA_sales)+sum(EU_sales)+sum(JP_sales)+sum(Other_sales))*100,2)||'%' as EU_sales
-,ROUND(sum(JP_sales)/(sum(NA_sales)+sum(EU_sales)+sum(JP_sales)+sum(Other_sales))*100,2)||'%' as JP_sales
-,ROUND(sum(Other_sales)/(sum(NA_sales)+sum(EU_sales)+sum(JP_sales)+sum(Other_sales))*100,2)||'%' as Other_sales
+select sum(NA_sales) as SUMMARY_NA_sales, sum(EU_sales) as SUMMARY_EU_sales, sum(JP_sales) as SUMMARY_JP_sales, sum(Other_sales) as SUMMARY_Other_sales from
+(select company
+,ROUND(sum(NA_sales))as NA_sales
+,ROUND(sum(EU_sales))as EU_sales
+,ROUND(sum(JP_sales)) as JP_sales
+,ROUND(sum(Other_sales)) as Other_sales
 from game
 inner join companies using(company_id)
 inner join sales using(game_rank)
-GROUP BY company;
+GROUP BY company);
 
 select realease_year as YEAR_OF_REALES, count(name_of_game) as AMOUNT_OF_REALESES
 from game
